9192d26f1e8c91ea608580365986af39b132df95
RockedPanda <chenghuawei@gmail.com>
2022-09-17 09:18:18 +0800

    初始版本

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # nrep
 text grep tool write by Node.js, 使用Node.js编写的类似grep的文本过滤工具
+
+
+## 使用说明
+
+* cat catlina.out |node nrep.js -p "x.length"
+* cat catlina.out |node nrep.js -p -c count.js
+* cat catlina.out |node nrep.js -p -c count.js -e "log(a,b,a/b)"
+
+
+## 外挂脚本
+-c sourceFile.js
+sourceFile.js中可以实现1-3个函数(匿名函数或者箭头函数均可)
+第一个表示行处理方法, 签名为: (x, lineIndex)=>{}
+第二个表示启动时预处理方法, 签名为: ()=>{}
+第三个表示结束时后处理方法, 签名为: ()=>{}
+如果是只有一个函数, 可以省略[]
+如果是多个函数, 文件内容格式应该是[func1, func2, func3]格式
---
+++ nrep.js
@@ -0,0 +1,107 @@
+const fs = require('fs');
+const readline = require('readline');
+const args = process.argv.slice(2);
+
+let i = 0;
+let sum = 0;
+let a=0,b=0,c=0,d=0,e=0,f=0,g={};
+
+let re = args[0]; //默认第一个参数是待执行的函数语句
+let autoLog = false;
+let sourceFile = '';
+let inputFile = '';
+let outputFile = '';
+let main = {
+    reFunc: x=>log(x),
+    startFunc: ()=>{},
+    endFunc: ()=>{},
+    enableTime: false,
+    timeStr:'min',
+    isTimeLine: false,
+    checkTimeLine:(x)=>{
+        this.isTimeLine = /2019|2020|2021/.test(x.slice(0,20)); //简单起见,按照前20字符包含2019或2020作为是否含时间的判定依据
+    }
+};
+
+args.forEach((arg,argIndex)=>{
+    // console.log(arg, argIndex);
+    if(arg=='-p'){
+        autoLog = true;
+    }else if(arg=='-s'){
+        let sFunc = args[argIndex+1];
+        if(sFunc.indexOf('=>')==-1){
+            sFunc = '()=>'+sFunc;
+        }
+        main.startFunc = eval(sFunc);
+    }else if(arg=='-e'){
+        let eFunc = args[argIndex+1];
+        if(eFunc.indexOf('=>')==-1){
+            eFunc = '()=>'+eFunc;
+        }
+        main.endFunc = eval(eFunc);
+    }else if(arg=='-t'){
+        main.enableTime = true;
+        main.timeStr = args[argIndex+1];
+    }else if(arg=='-c'){ //code sourceFile
+        sourceFile = args[argIndex+1];
+        // console.log(sourceFile);
+        re = '('+fs.readFileSync(sourceFile,'utf8')+')';
+        // console.log(re);
+    }else if(arg=='-i'){ //input file
+        inputFile = args[argIndex+1];
+    }/* else if(arg=='-o'){ //output file
+        outputFile = args[argIndex+1];
+    } */
+});
+if(args.length === 0){
+    console.log('需要输入过滤方法');
+    return;
+}
+if(autoLog && !sourceFile){
+    re = `log(${re})`;
+}
+
+if(re.indexOf('=>')==-1 && !sourceFile){
+    re = `(x)=>{${re}}`;
+}
+
+function log(){
+    console.log.apply(console, arguments);
+}
+// console.log('>>>>'+re+'<<<<');
+main.reFunc = eval(re);
+if(Array.isArray(main.reFunc)){
+    main.startFunc = main.reFunc[1] || main.startFunc;
+    main.endFunc = main.reFunc[2] || main.endFunc;
+    main.reFunc = main.reFunc[0] || main.reFunc;
+}
+
+main.startFunc();
+const rl = readline.createInterface({
+  input: inputFile? fs.createReadStream(inputFile) : process.stdin,
+  output: process.stdout,
+  terminal: false,
+  crlfDelay: Infinity
+});
+
+rl.on('line', (line) => {
+    if(main.enableTime){
+        main.checkTimeLine(line);
+    }
+  main.reFunc(line, i);
+  i++;
+});
+
+rl.on('close',()=>{
+    main.endFunc();
+});
+
+/** sourceFile内容格式
+ * function(x, rowIndex){
+ *    //一般使用x代替行内容
+ *    //可以使用的全局变量包括: a=0,b=0,c=0,d=0,e=0,f=0,g={}
+ *    //临时内容可以存放到g对象的自定义属性下
+ *    //函数不一定有返回值
+ *    //可以在函数中使用log或者console.log输出日志
+ * }
+ */
